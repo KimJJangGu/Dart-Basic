be730a2549b010b7c982b296d3e6cb513488277d
Kim DoHyeon <dohyun51731@gmail.com>
2023-11-30 22:53:09 +0900

    231130 과제

--- lib/23_11_30/cleric_4.dart
+++ lib/23_11_30/cleric_4.dart
@@ -2,32 +2,52 @@ import 'dart:math';
 
 class Cleric{
   String name;
-  int hp;
-  int mp;
-  final maxhp;
-  final maxmp;
+  int hp = 50;
+  int mp = 10;
+  final maxhp = 50;
+  final maxmp = 10;
 
 
-  Cleric(this.name, this.hp, this.maxhp, this.mp, this.maxmp);
+  Cleric(this.name);
+
 
   void selfAid(){
-    hp = 50;
-    print('$name은 MP를 5 소비하고, HP를 최대로 회복했다.');
+    if(mp < 5){
+      print('MP가 부족합니다.');
+    } else{
+      print('MP를 5 소비하고, HP를 최대로 회복했다. \nHP: $maxhp, MP: $mp');
+      mp -= 5;
+    }
   }
 
-  int heal = Random().nextInt(3);
-  int sec =  Random().nextInt(4);
 
-  void pray(){
-    print('$name은 $sec초 기도하여 MP를 $heal만큼 회복했다.');
+  int pray(int sec){
+    int heal = Random().nextInt(3);
+    int lastmp = 0;
+    mp += heal + sec;
+    if(mp <= 10){
+      print('$sec초 기도하여 MP를 ${heal + sec}만큼 회복했다.\n현재 MP: ${mp}');
+    } else{
+      print('$sec초 기도하여 ${lastmp}만큼 회복, MP를 현재 MP: $maxmp, 최대치입니다.');
+    }
+
+    return lastmp;
   }
 }
 
 void main(){
-  Cleric cleric = Cleric('김도현', 50, 50, 10, 10);
+  Cleric cleric = Cleric('고블린');
   print(cleric);
 
   cleric.selfAid();
+  cleric.selfAid();
+  cleric.selfAid();
 
-  cleric.pray();
+  cleric.pray(1);
+  cleric.pray(1);
+  cleric.pray(1);
+  cleric.pray(1);
+  cleric.pray(1);
+  cleric.pray(1);
+  cleric.pray(1);
 }
