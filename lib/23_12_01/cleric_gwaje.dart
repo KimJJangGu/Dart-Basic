4ff25f5fc6cbe89a6d03d83d5ae99f17d3142108
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-01 21:17:55 +0900

    231201 과제

--- lib/23_12_01/cleric_gwaje.dart
+++ lib/23_12_01/cleric_gwaje.dart
@@ -1,24 +1,26 @@
 // 2-1 최대 HP, 최대 MP의 필드가 각 인스턴스별로 있지 않도록, 필드 선언에 적절한 키워드를 추가
-final int maxHP = 50;
-final int maxMP = 10;
-
 class Cleric {
   String name;
   int hp = 50;
   int mp = 10;
 
-  // static이란 일반 객체를 만들 때 같이 메모리에 저장하는 것이 아닌
-  // 따로 메모리를 두어 static 구문을 객체가 공유하여 사용할 수 있게 해주는 구문
+  // const로 값을 먼저 지정해야 됨
+  static const int maxHP = 50;
+  static const int maxMP = 10;
+
+  // static이란 일반 객체를 만들 때 같이 메모리에 저장하는 것이 아닌 따로 메모리를 둠
+  // static 구문을 객체가 공유하여 사용할 수 있게 해주는 구문
 
   // static 키워드는 변수나 메소드에 사용
   // static 키워드를 사용하면 클래스가 메모리에 로딩될 때 자동으로 생성
   // static은 instance에 귀속되지 않고 class에 귀속
   // 객체를 생성하지 않아도 사용이 가능하며, 속도가 빠르다
 
-  int maxHP = 50;
-  int maxMP = 10;
+  Cleric(this.name, {this.hp = maxHP, this.mp = maxMP});
 
-  Cleric(this.name, {this.hp = 50, this.mp = 10});
+  // {} 으로 감싸져 있다.
+  // {} 안에 있는 값은 옵셔널이다.
+  // 값을 할당하려면 ":" 을 꼭 붙여야 한다.
 }
 
 void main() {
@@ -32,9 +34,4 @@ void main() {
 
   // Cleric()과 같이 이름을 지정하지 않는 경우 인스턴스화 할 수 없다
   // final cleric4 = Cleric();
-
-  print(cleric1);
-  print(cleric2);
-  print(cleric3);
-  //print(cleric4);
 }
