4b0adf91316cc86d77a2521d431188ff0b535a93
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-08 16:03:55 +0900

    231208 연습문제(일단 써)

--- lib/23_12_08/과제/strong_box.dart
+++ lib/23_12_08/과제/strong_box.dart
@@ -1,6 +1,6 @@
 // 다음 조건을 만족하는 금고인 StrongBox 클래스를 정의
+// 금고 클래스에 담는 인스턴스의 타입은 미정
 class StrongBox<E> {
-  // 금고 클래스에 담는 인스턴스의 타입은 미정
   E? _present;
 
   // put() 메서드로 인스턴스를 저장
@@ -8,6 +8,16 @@ class StrongBox<E> {
 
   // get() 메서드로 인스턴스를 얻을 있음
   E? get() => _present;
+}
+
+// 열거형 KeyType을 정의, 각 열쇠는 사용횟수의 한도가 정해져 있다
+// 열쇠의 종류를 나타내는 필드 변수
+// 열쇠의 종류를 받는 생성자
+enum KeyType {
+  padlock, // 1,024회
+  button, // 10,000회
+  dial, // 30,000회
+  finger, // 1,000,000회
 }
 
 void main() {
